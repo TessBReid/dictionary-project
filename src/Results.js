71ab490a84bd07771bd0b849c2f0567cdffcf74c
TessBReid <101984008+TessBReid@users.noreply.github.com>
2022-08-04 18:28:40 -0400

    Created word meaning loop

--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 import "./Results.css";
 
 export default function Results(props) {
-  console.log(props.result);
   if (props.result) {
     return (
       <div className="Results">
         <h3>{props.result.word}</h3>
         {props.result.meanings.map(function (meaning, index) {
-          return meaning.definitions[0].definition;
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
